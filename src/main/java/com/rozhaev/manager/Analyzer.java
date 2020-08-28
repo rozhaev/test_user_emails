3ec980dcd369fa92f66b83babeedb49a6459b8d1
Dmitry Rozhaev <dmitry.rozhaev@gmail.com>
2020-08-28 23:15:52 +0300

    created Analyzer for algorithm

---
+++ src/main/java/com/rozhaev/manager/Analyzer.java
@@ -0,0 +1,9 @@
+package com.rozhaev.manager;
+
+import java.util.Map;
+import java.util.Set;
+
+public interface Analyzer {
+
+    Map<String, Set<String>> run();
+}
---
+++ src/main/java/com/rozhaev/manager/AnalyzerImpl.java
@@ -0,0 +1,11 @@
+package com.rozhaev.manager;
+
+import java.util.Map;
+import java.util.Set;
+
+public class AnalyzerImpl implements Analyzer{
+    @Override
+    public Map<String, Set<String>> run() {
+        return null;
+    }
+}
