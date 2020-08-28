92e5dcb965fb60d18ab428998c020dc77d0be468
Dmitry Rozhaev <dmitry.rozhaev@gmail.com>
2020-08-28 23:33:07 +0300

    created Analyzer to check emails

--- src/main/java/com/rozhaev/Main.java
+++ src/main/java/com/rozhaev/Main.java
@@ -1,8 +1,18 @@
 package com.rozhaev;
 
+import com.rozhaev.manager.Analyzer;
+import com.rozhaev.manager.AnalyzerImpl;
+
+import java.util.Map;
+import java.util.Set;
+
 public class Main {
 
     public static void main(String[] args){
-        System.out.println("Test");
+        Analyzer analyzer = new AnalyzerImpl();
+        Map<String, Set<String>> userEmails = analyzer.run();
+
+        System.out.println("Result:");
+        userEmails.forEach((userName, emails) -> System.out.println(userName + String.join(", ", emails)));
     }
 }
